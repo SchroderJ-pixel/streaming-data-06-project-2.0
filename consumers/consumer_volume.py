a41373fdebf35fb7e797957c501c82aeb90a8516
Justin Schroder <jdschroder18@gmail.com>
2025-09-29 09:56:17 -0500

    consumer_volume.py is working.

--- consumers/consumer_volume.py
+++ consumers/consumer_volume.py
@@ -1,5 +1,15 @@
-import orjson
+# --- project root on sys.path + six shim (Windows fix) ---
+import sys, six
+from pathlib import Path
+ROOT = Path(__file__).resolve().parent.parent
+if str(ROOT) not in sys.path:
+    sys.path.insert(0, str(ROOT))
+sys.modules['kafka.vendor.six'] = six
+sys.modules['kafka.vendor.six.moves'] = six.moves
+# ----------------------------------------------------------
+
 from kafka import KafkaConsumer
+import orjson
 from utils.utils_config import KAFKA_BROKER, KAFKA_TOPIC
 from utils.utils_logger import get_logger
 from utils.utils_state import update_volume
